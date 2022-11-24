1c072a19041a4bec2af0506cd1bb4e02ad32d579
SarahVestergren <sscv@itu.dk>
2022-11-24 09:58:58 +0100

    Made new statement

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Måske sker der fejl");
-        System.out.println("Hello!!");
-        System.out.println("Hej!!");
+        System.out.println("This is our main");
     }
 }
