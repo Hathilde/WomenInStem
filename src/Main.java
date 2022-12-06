966d1d566b27d08fa86cccaee9751d0269ec338c
Hathilde <mafb@itu.dk>
2022-12-06 09:38:13 +0100

    GUI der virker på Mathildes computer 6/12

--- src/Main.java
+++ src/Main.java
@@ -13,9 +13,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(Main.class.getResource("FXView.fxml"));
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("FView.fxml"));
         Scene scene = new Scene(loader.load());
         stage.setScene(scene);
         stage.show();
+        stage.setTitle("PseudoFlix");
     }
 }
