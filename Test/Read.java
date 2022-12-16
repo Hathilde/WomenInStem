d19427481cb5740a3cea882677441af60ee07eaf
AnnikaJensby <118458497+AnnikaJensby@users.noreply.github.com>
2022-12-16 09:34:44 +0100

    Opdateret version Annika 16.27 , exceptions added til Mathildes rettede kode.

--- Test/Read.java
+++ Test/Read.java
@@ -12,12 +12,14 @@ public class Read {
         ReadData dataReader = new ReadData();
         dataReader.reader("./film.txt");
         dataReader.reader("./serier.txt");
-      // assert(dataReader.getSortedMediaObjects().get(99), "Titlen er Yankee Doodle Dandy. Året den er lavet er 1942. Genren er [ Biography,  Drama,  Musical]. IMDB-ratingen er 7,7."");
+
 
         System.out.println(dataReader.getAllMediaObjects().get(99).getImgPath());
-        //System.out.println(dataReader.getSortedMediaObjects());
+
 
     }
+
+
     @Test
     public void testSpecificDataOfMedia() {
         ReadData dataReader = new ReadData();
